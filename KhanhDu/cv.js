3f00953b422fddc9d171e9ab55bf26c8338934f2
tuyenlt211 <tuyenlt1455@ut.edu.vn>
2025-06-22 23:54:38 +0700

    Add files via upload

---
+++ KhanhDu/cv.js
@@ -0,0 +1,12 @@
+// Ví dụ hiệu ứng cuộn đến phần
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute("href"));
+    if (target) {
+      target.scrollIntoView({
+        behavior: "smooth"
+      });
+    }
+  });
+});
